063aa79e7f01d580d75a31bf3678213ec0ef7e5f
Sergio Remedio <sreme002@fiu.edu>
2018-10-29 02:51:16 -0400

    updated master

--- facebookScripts.js
+++ facebookScripts.js
@@ -1,55 +1,62 @@
-//loads and initialize the SDK of facebook
+
+
+
+///////////////////////////////////////////loads and initialize the SDK of facebook
 (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
+  console.log("Did I get here ???");
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) { return; }
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
 
 function logIn() {
-    FB.login(function(response) {
-        if (response.authResponse) {
-         console.log('Welcome!  Fetching your information.... ');
-         FB.api('/me', function(response) {
-           console.log('Good to see you, ' + response.name + '.');
-         });
-        } else {
-         console.log('User cancelled login or did not fully authorize.');
-        }
-    });
-
-    /*
-    Things to do:
-
-    -payment method
-    -redirect user if connected to course page 
-    -start developing the course page
-    
-    */
+  FB.login(function (response) {
+    if (response.authResponse) {
+      console.log('Welcome!  Fetching your information.... ');
+      FB.api('/me', function (response) {
+        console.log('Good to see you, ' + response.name + '.');
+        /*
+            After log in, we go to content page
+        */
+      });
+    } else {
+      console.log('User cancelled login or did not fully authorize.');
+    }
+  });
 }
+/*
+  Things to do:
 
+  -payment method 
+  -start developing the course page
+  
+  */
 
 window.fbAsyncInit = function () {
-    FB.init({
-        appId: '1686727554790401',
-        cookie: true,
-        xfbml: true,
-        version: 'v3.1'
-    });
+  FB.init({
+    appId: '1686727554790401',
+    cookie: true,
+    xfbml: true,
+    version: 'v3.1'
+  });
 
-    FB.AppEvents.logPageView();
+  FB.AppEvents.logPageView();
 
 };
 
 
 function makeAccount() {
-    window.location.href = ("http://facebook.com");// redirect top facebook page to make account
+  window.location.href = ("http://facebook.com");// redirect top facebook page to make account
 }
 
-function request(){
-    //send email to Mr.Erietta?
+function request() {
 
-    console.log("Request sent");
+console.log("Requested");
+
 }
+
+
+
